5c8cb7a77ba65079b2af110d2e2cbb3f3c1f7303
HandralJadesha <117385783+HandralJadesha@users.noreply.github.com>
2024-08-23 05:57:56 +0000

    container config done

---
+++ Basics/conversationOperation.js
@@ -0,0 +1,29 @@
+let score=null
+// console.log(typeof score)
+
+let valueInNumber=Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+//***** NUMBER *****
+// "33" => 33
+// "33abc" => NaN
+// true =>1 ; false=> 0
+// null => 0
+// undefined => NaN
+
+//***** BOOLEAN *****
+let isLoggedIn = "Handral"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+//1 => true ; 0 => false
+// ""=> false ; "Handral"=>true
+
+//***** STRING *****
+let someNumber=77
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
