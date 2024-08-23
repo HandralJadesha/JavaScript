a7f21e3c49223521458cf1846296709aaee16cc8
HandralJadesha <117385783+HandralJadesha@users.noreply.github.com>
2024-08-23 06:19:00 +0000

    conversationOperation

--- Basics/conversationOperation.js
+++ Basics/conversationOperation.js
@@ -25,5 +25,40 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 //***** STRING *****
 let someNumber=77
 let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//************************** Operations **************** 
+
+let value=3
+let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1="Hello"
+let str2=" Handral"
+let str3=str1+str2
+// console.log(str3)
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(true) // true
+// console.log(+true) //1 because of increment
+// console.log(true+) // Error
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)
+
+
+
+
