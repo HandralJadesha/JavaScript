e9b7663db1dedc3cbde2607f7f8178dbae8e32d1
HandralJadesha <117385783+HandralJadesha@users.noreply.github.com>
2024-08-23 05:34:33 +0000

    container config done

---
+++ Basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("Hello") //we are using nodejs not browser
+
+console.log(3+3) //code readability should be high
+
+console.log("Handral")
+
+let name="Handral" //String
+let age=18 //Number
+let isLoggedIn=false //boolean
+
+// number => 2^53 range
+// bigInt => StockMarket
+// string => ""
+// boolean => true/false
+// null => standalone value//empty value representation let state=null
+// undefined => let state; no value is assigned
+// symbol => uniqueness 
+
+// object 
+
+console.log(typeof"Handral"); // string
+console.log(typeof 17)        // number
+console.log(typeof null)      // object
+console.log(typeof undefined)  // undefined
+
