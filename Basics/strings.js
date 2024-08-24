802d81e6310ba831e93de3e1d731210f4774ea4e
HandralJadesha <117385783+HandralJadesha@users.noreply.github.com>
2024-08-24 04:51:05 +0000

    Strings

---
+++ Basics/strings.js
@@ -0,0 +1,35 @@
+const name="Handral Jadesha "
+const repoCount=7
+
+//console.log(name + repoCount+" Of JS")
+
+// console.log(`Hello My name is ${name} and my repo count is ${repoCount}`)
+
+const newgame=new String('Handral')
+// console.log(newgame[0])
+// console.log(typeof newgame)
+// console.log(newgame.__proto__)
+// console.log(newgame.length)
+// console.log(newgame.toUpperCase())
+// console.log(newgame.charAt(2))
+// console.log(newgame.indexOf('d'))
+
+const game1=newgame.substring(0,4)
+// console.log(game1)
+
+const game2=newgame.slice(-7,4) //can give negative value
+// console.log(game2)
+
+const game3="  Handral Jadesha  "
+// console.log(game3)
+// console.log(game3.trim())  // it removes starting space and end space
+
+const url="https://HandralJadesha.com/handral"
+
+// console.log(url.replace('handral','jadesha'))
+// console.log(url.includes('handral'))
+
+
+const game4=new String('Handral Jadesha')
+const new4=game4.split(' ')
+console.log(new4)
